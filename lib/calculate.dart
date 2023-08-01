2a616997e7e1ae42e96aacaf8cd77999f81db835
yunusemrekaplan <92227265+yunusemrekaplan@users.noreply.github.com>
2023-08-02 02:24:59 +0300

    .

---
+++ lib/calculate.dart
@@ -0,0 +1,14 @@
+import 'package:kuyumcu_stok/enum_carat.dart';
+import 'package:kuyumcu_stok/services/gold_service.dart';
+
+class Calculate {
+  static Map<String, double> calculateCostPrice(Carat carat, double gram) {
+    double mil = carat.milDefinition;
+    double costGram = (carat.intDefinition + mil) * gram;
+    double costPrice = costGram * GoldService.fGold;
+    return {
+      'costGram': costGram,
+      'costPrice': costPrice,
+    };
+  }
+}
