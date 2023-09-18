dd162a60f00ea6dbb18883767a580add73d54bad
yunusemrekaplan <yunusemrekaplan1@hotmail.com>
2023-09-18 12:42:47 +0300

    .

--- lib/line_chart.dart
+++ lib/line_chart.dart
@@ -5,35 +5,109 @@ import 'package:flutter/material.dart';
 import 'package:kuyumcu_stok/app_colors.dart';
 
 class LineChartSample2 extends StatefulWidget {
-  LineChartSample2({super.key, required this.spotList});
+  LineChartSample2({
+    super.key,
+    required this.spotList,
+    required this.days,
+    required this.values,
+    required this.chartName,
+  });
 
+  late String chartName;
   late List<FlSpot> spotList;
+  late List<String> days;
+  late List<String> values;
 
   @override
   State<LineChartSample2> createState() => _LineChartSample2State();
 }
 
 class _LineChartSample2State extends State<LineChartSample2> {
+  late List<FlSpot> avgSpotList;
+  late double avg;
+
   List<Color> gradientColors = [
     Colors.yellow,
     Colors.green,
   ];
 
+  bool showAvg = false;
+
+  @override
+  void initState() {
+    avgSpotList = [];
+
+    if (widget.spotList.isNotEmpty) {
+      double temp = 0;
+      for (var element in widget.spotList) {
+        temp += element.y;
+      }
+
+      avg = temp / widget.spotList.length;
+      List.generate(widget.spotList.length, (i) {
+        avgSpotList.add(FlSpot(i.toDouble(), avg));
+      });
+    }
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.70,
-      child: Padding(
-        padding: const EdgeInsets.only(
-          right: 18,
-          left: 12,
-          top: 24,
-          bottom: 12,
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  showAvg = false;
+                });
+              },
+              child: Text(
+                widget.chartName,
+                style: TextStyle(
+                  fontSize: 28,
+                  color: showAvg ? Colors.white.withOpacity(0.5) : Colors.white,
+                ),
+              ),
+            ),
+            const Text(
+              '-',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  showAvg = true;
+                });
+              },
+              child: Text(
+                'Ort',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: showAvg ? Colors.white : Colors.white.withOpacity(0.5),
+                ),
+              ),
+            ),
+          ],
         ),
-        child: LineChart(
-          mainData(),
+        AspectRatio(
+          aspectRatio: 1.70,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              right: 18,
+              left: 12,
+              top: 24,
+              bottom: 12,
+            ),
+            child: LineChart(
+              showAvg ? avgData() : mainData(),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 
@@ -115,71 +189,7 @@ class _LineChartSample2State extends State<LineChartSample2> {
     );
   }
 
-  Widget bottomTitleWidgets(double value, TitleMeta meta) {
-    const style = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 16,
-      color: Colors.white,
-    );
-    Widget text;
-    switch (value.toInt()) {
-      case 0:
-        text = const Text('Pzt', style: style);
-        break;
-      case 1:
-        text = const Text('Salı', style: style);
-        break;
-      case 2:
-        text = const Text('Çar', style: style);
-        break;
-      case 3:
-        text = const Text('Per', style: style);
-        break;
-      case 4:
-        text = const Text('Cuma', style: style);
-        break;
-      case 5:
-        text = const Text('Cmt', style: style);
-        break;
-      case 6:
-        text = const Text('Pzr', style: style);
-        break;
-      default:
-        text = const Text('', style: style);
-        break;
-    }
-
-    return SideTitleWidget(
-      axisSide: meta.axisSide,
-      child: text,
-    );
-  }
-
-  Widget leftTitleWidgets(double value, TitleMeta meta) {
-    const style = TextStyle(
-      fontWeight: FontWeight.bold,
-      fontSize: 15,
-      color: Colors.white,
-    );
-    String text;
-    switch (value.toInt()) {
-      case 1:
-        text = '10K';
-        break;
-      case 3:
-        text = '30k';
-        break;
-      case 5:
-        text = '50k';
-        break;
-      default:
-        return Container();
-    }
-
-    return Text(text, style: style, textAlign: TextAlign.left);
-  }
-
-  /*LineChartData avgData() {
+  LineChartData avgData() {
     return LineChartData(
       gridData: FlGridData(
         show: true,
@@ -210,7 +220,7 @@ class _LineChartSample2State extends State<LineChartSample2> {
         bottomTitles: AxisTitles(
           sideTitles: SideTitles(
             showTitles: true,
-            reservedSize: 50,
+            reservedSize: 30,
             interval: 1,
             getTitlesWidget: bottomTitleWidgets,
           ),
@@ -226,7 +236,7 @@ class _LineChartSample2State extends State<LineChartSample2> {
       ),
       borderData: FlBorderData(
         show: true,
-        border: Border.all(color: const Color(0xff37434d)),
+        border: Border.all(color: Colors.white38),
       ),
       minX: 0,
       maxX: 6,
@@ -234,13 +244,7 @@ class _LineChartSample2State extends State<LineChartSample2> {
       maxY: 6,
       lineBarsData: [
         LineChartBarData(
-          spots: const [
-            FlSpot(0, 2),
-            FlSpot(1.5, 2.6),
-            FlSpot(3, 4.3),
-            FlSpot(4.5, 4.0),
-            FlSpot(6, 5),
-          ],
+          spots: avgSpotList,
           isCurved: true,
           gradient: LinearGradient(
             colors: gradientColors,
@@ -261,5 +265,72 @@ class _LineChartSample2State extends State<LineChartSample2> {
         ),
       ],
     );
-  }*/
+  }
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+      color: Colors.white,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 0:
+        text = Text(widget.days[0], style: style);
+        break;
+      case 1:
+        text = Text(widget.days[1], style: style);
+        break;
+      case 2:
+        text = Text(widget.days[2], style: style);
+        break;
+      case 3:
+        text = Text(widget.days[3], style: style);
+        break;
+      case 4:
+        text = Text(widget.days[4], style: style);
+        break;
+      case 5:
+        text = Text(widget.days[5], style: style);
+        break;
+      case 6:
+        text = Text(widget.days[6], style: style);
+        break;
+      default:
+        text = const Text('', style: style);
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      child: text,
+    );
+  }
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 15,
+      color: Colors.white,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = widget.values[0];
+        break;
+      case 2:
+        text = widget.values[2];
+        break;
+      case 4:
+        text = widget.values[4];
+        break;
+      case 6:
+        text = widget.values[6];
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(text, style: style, textAlign: TextAlign.left);
+  }
 }
